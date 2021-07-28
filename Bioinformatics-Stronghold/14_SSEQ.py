3a4f5a89835fda095a57bea0075c71e77c76ac7b
JonAbebe <31824906+JonAbebe@users.noreply.github.com>
2021-07-28 19:09:42 -0400

    Update 14_SSEQ.py

--- Bioinformatics-Stronghold/14_SSEQ.py
+++ Bioinformatics-Stronghold/14_SSEQ.py
@@ -6,6 +6,7 @@ Given: Two DNA strings s and t (each of length at most 1 kbp) in FASTA format.
 
 Return: One collection of indices of s in which the symbols of t appear as a subsequence of s. If multiple solutions exist, you may return any one.
 """
+
 from helper_scripts.argument_parsing import parser
 from helper_scripts.output_text import write_to_file
 from helper_scripts.read_fasta import read_fasta
